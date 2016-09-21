c69f2d1d10259b761745e44f2e6a3c518f655877
AJ Heller <aj@drfloob.com>
2016-09-21 10:17:20 -0700

    add kafka topic creation script, for reference

---
+++ ec2.setup/kafka-create-pulse-topics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1 minute retention, GH firehose stream
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 \
+--partitions 3 --config retention.ms=60000 --topic gh_fat_topic
+
+# 30 second retention, GH skinny stream
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 \
+--partitions 3 --config retention.ms=30000 --topic gh_skinny_topic
+
