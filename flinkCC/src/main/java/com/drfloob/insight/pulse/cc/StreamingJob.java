4c1652b6dc5e424420dbd8f83eabcc4963a3d6a8
AJ Heller <aj@drfloob.com>
2016-09-22 09:44:59 -0700

    remove redundant windowing on flinkCC; upgrade flink kafka libs

--- flinkCC/src/main/java/com/drfloob/insight/pulse/cc/StreamingJob.java
+++ flinkCC/src/main/java/com/drfloob/insight/pulse/cc/StreamingJob.java
@@ -3,6 +3,7 @@ package com.drfloob.insight.pulse.cc;
 import com.drfloob.insight.pulse.cc.schema.skinny.SkinnyGHRecord;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.graph.Edge;
 import org.apache.flink.graph.streaming.SimpleEdgeStream;
@@ -12,16 +13,12 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.IngestionTimeExtractor;
-import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
-import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer082;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer08;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
 import org.apache.flink.util.Collector;
 
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 
 public class StreamingJob {
@@ -36,7 +33,7 @@ public class StreamingJob {
         DatumReader<SkinnyGHRecord> reader = new SpecificDatumReader<SkinnyGHRecord>(SkinnyGHRecord.class);
 
         AvroDeserializationSchema<SkinnyGHRecord> skinnySchema = new AvroDeserializationSchema<>(SkinnyGHRecord.class);
-        FlinkKafkaConsumer082<SkinnyGHRecord> kafkaConsumer = new FlinkKafkaConsumer082<>("gh_skinny_topic", skinnySchema, flinkProps);
+        FlinkKafkaConsumer08<SkinnyGHRecord> kafkaConsumer = new FlinkKafkaConsumer08<>("gh_skinny_topic", skinnySchema, flinkProps);
 		DataStream<SkinnyGHRecord> skinnyStream = env.addSource(kafkaConsumer);
 
         SingleOutputStreamOperator<SkinnyGHRecord> sos =  skinnyStream.assignTimestampsAndWatermarks(new IngestionTimeExtractor<SkinnyGHRecord>());
@@ -52,25 +49,17 @@ public class StreamingJob {
                 }
         ), env);
 
-        // 1 hour window to start, emitted every second
+        // 1/2 second processing time window
         Long windowLength = 500L;
         DataStream<DisjointSet<String>> cc = ses.aggregate(new WindowedConnectedComponents<String, SkinnyGHRecord>(windowLength));
-
-//        cc.timeWindowAll(Time.of(windowLength, TimeUnit.MILLISECONDS))
-//                .apply(new AllWindowFunction<DisjointSet<String>, String, TimeWindow>() {
-//                    @Override
-//                    public void apply(TimeWindow window, Iterable<DisjointSet<String>> values, Collector<String> out) throws Exception {
-//                        for (DisjointSet<String> djs : values)
-//                            out.collect(djs.toString());
-//                    }
-//                }).addSink(new FlinkKafkaProducer08<String>("gh_components", new SimpleStringSchema(), flinkProps));
 
         cc.map(new MapFunction<DisjointSet<String>, String>() {
             @Override
             public String map(DisjointSet<String> value) throws Exception {
-                   return value.toString();
+                return value.toString();
             }
-        }).addSink(new FlinkKafkaProducer08<String>("gh_components", new SimpleStringSchema(), flinkProps));
+        })
+        .addSink(new FlinkKafkaProducer08<String>("gh_components", new SimpleStringSchema(), flinkProps));
 
         // execute program
 		env.execute("Flink Streaming Connected Components");
