742fe8b1214803de7ba2a243973eec83c50642c4
AJ Heller <aj@drfloob.com>
2016-09-27 12:52:00 -0700

    kafka: tweak gh_fat_topic retention policy for live demos
    For partition tolerance, more storage should be added to nodes, and
    the retention policy should be increased significantly. Also, adding a
    long-term persistence sink from each topic would probably be a great
    idea.

--- ec2.setup/kafka-create-pulse-topics.sh
+++ ec2.setup/kafka-create-pulse-topics.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# 1 minute retention, GH firehose stream
+# for fault tolerance, I'd beef up the disk storage and increase the
+# retention significantly. I'd also potentially sink directly to long
+# term storage.
+
+# 30 second retention, GH firehose stream
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 \
---partitions 3 --config retention.ms=60000 --topic gh_fat_topic
+--partitions 3 --config retention.ms=30000 --topic gh_fat_topic
 
 # 30 second retention, GH skinny stream
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 2 \
