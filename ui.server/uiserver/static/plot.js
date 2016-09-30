77de7cc52f3c5e3f4df049d9cc5c02f5cfcb9799
AJ Heller <aj@drfloob.com>
2016-09-30 16:28:05 -0700

    ui: fix the bar chart, make it dynamic

--- ui.server/uiserver/static/plot.js
+++ ui.server/uiserver/static/plot.js
@@ -5,12 +5,36 @@ var plotData = [{
     orientation: 'h'
 }];
 
-Plotly.newPlot('barChart', plotData);
+var plotlyLayout = {xaxis: {range: [0,1000]}};
+
+Plotly.newPlot('barChart', plotData, layout);
 
 function updatePlot(data) {
-    var counts = _.reduce(data, function(one, agg) {
-	agg[one.length]++;
+    console.log('updatePlot: data: ', data);
+    if (_.size(_.keys(data)) > 1000) {
+	alert("NOPE!")
+	return;
+    }
+    var counts = _.reduce(data, function(agg, one) {
+	var len = _.size(_.filter(one, function(e) {return e != null && e != "" && (!_.isNumber(e) || !isNaN(e))}))
+	var idx = 0;
+	if (len == 2)
+	    idx = 0;
+	else if (len <= 4)
+	    idx = 1;
+	else if (len <= 9)
+	    idx = 2;
+	else
+	    idx = 3;
+	
+	agg[idx]++;
 	return agg;
     }, [0,0,0,0]);
-    Plotly.restyle('barChart', 'x', counts);
+    console.log('counts', counts);
+    Plotly.newPlot('barChart', [{
+	type: 'bar',
+	x: counts,
+	y: ['2', '3-4', '5-9', '10+'],
+	orientation: 'h'
+    }], plotlyLayout);
 }
